43c55d7699c92e34230827b3d6b1f48b006d964f
valerosoo <francisco.negri2.0@gmail.com>
2026-03-16 11:58:31 -0300

    agregue que se pueda entrar al menu de opciones y que la escena de inicializacion del proyecto sea menu

--- pong/scripts/game_2p.gd
+++ pong/scripts/game_2p.gd
@@ -17,10 +17,14 @@ func _process(delta: float) -> void:
 		print("Gano der")
 		get_tree().paused = true
 		get_node("GanadorJ2").visible = true
+		await get_tree().create_timer(3).timeout
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
 	elif score_izq == 10:
 		print("Gano izq")
 		get_tree().paused = true
 		get_node("GanadorJ1").visible = true
+		await get_tree().create_timer(3).timeout
+		get_tree().change_scene_to_file("res://scenes/menu.tscn")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Pausa"):
---
+++ pong/scripts/opciones.gd
@@ -0,0 +1,17 @@
+extends Label
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		print("Opciones")
+		get_tree().change_scene_to_file("res://scenes/opciones.tscn")
