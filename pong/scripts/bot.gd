b1de12f0a7e441fc0ab3a958894d6851e6731ec2
valerosoo <francisco.negri2.0@gmail.com>
2026-03-17 16:55:15 -0300

    no habia guardado las files del commit anterior

--- pong/scripts/bot.gd
+++ pong/scripts/bot.gd
@@ -10,21 +10,25 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	if _pelota._direction.x > 0 and _pelota.position.x < position.x:
-		var tiempo = (_pelota.position.x - position.x) / (_pelota._direction.x * _pelota._speed)
+	var altura = get_viewport_rect().size.y
+	print("altura" + str(altura))
+	if _pelota._direction.x > 0:
+		var tiempo = (position.x - _pelota.position.x) / (_pelota._direction.x * _pelota._speed)
 		var y_futura = _pelota.position.y + _pelota._direction.y * _pelota._speed * tiempo
 			
-		while y_futura < 0 or y_futura > 720:
+		while y_futura < 0 or y_futura > altura:
 			if y_futura < 0:
 				y_futura = -y_futura
-			elif y_futura > 720:
-				y_futura = 2*720-y_futura
+			elif y_futura > altura:
+				y_futura = 2*altura-y_futura
 		y_futura+=randf_range(-20,20)
 		var error = y_futura-position.y
 		position.y += clamp(error, -_speed * delta, _speed * delta)
 			
 	else: 
-		position.y += clamp(0, -_speed * delta, _speed * delta)
+		var target = altura / 2
+		var error = target - position.y
+		position.y += clamp(error, -_speed * delta, _speed * delta)
 	
 	move_and_slide()
 		
